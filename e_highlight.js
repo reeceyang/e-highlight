3e3f46ae63ffd22c7ab9c73dbf9497e73adae5d2
Reece Yang <reeceyang@live.com>
2017-04-01 15:56:01 -0500

    Update e_highlight.js

--- e_highlight.js
+++ e_highlight.js
@@ -3,7 +3,7 @@ var line;
 var oldLength = 0;
 var input = document.getElementById("text");
 var out = document.getElementById("output");
-input.addEventListener("text", function() {
+input.addEventListener("input", function() {
     line = input.innerHTML;
     out.innerHTML = out.innerHTML.substring(0, out.innerHTML.length-oldLength);
     out.innerHTML += line;
