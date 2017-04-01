17dc4b2025e9afacaf98980c8583ee9d3b7d8f61
Reece Yang <reeceyang@live.com>
2017-04-01 15:32:42 -0500

    Update e_highlight.js

--- e_highlight.js
+++ e_highlight.js
@@ -2,4 +2,5 @@ console.log("script loaded");
 while (true) {
   var text = document.getElementById("text").innerHTML;
   text.replace(/e/g, "<span style=\"text-highlight:green\"\">e</span>");
+  document.getElementById("text").innerHTML = text;
 }
