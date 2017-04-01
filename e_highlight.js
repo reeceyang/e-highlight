0e4554e78eb254f341caed2738e13bb6365c9792
Reece Yang <reeceyang@live.com>
2017-04-01 15:44:16 -0500

    Update e_highlight.js

--- e_highlight.js
+++ e_highlight.js
@@ -1,6 +1,16 @@
 console.log("script loaded");
-while (true) {
+var line;
+var oldLength = 0;
+var input = document.getElementById("text");
+var out = document.getElementById("output");
+input.addEventListener("text", function() {
+    line = input.innerHTML;
+    out.innerHTML = out.innerHTML.substring(0, out.innerHTML.length-oldLength);
+    out.innerHTML += line;
+    oldLength = line.length;
   var text = document.getElementById("text").innerHTML;
-  text.replace(/e/g, "<span style=\"text-highlight:green\"\">e</span>");
-  document.getElementById("output").innerHTML = text;
-}
+text.replace(/e/g, "<span style=\"text-highlight:green\"\">e</span>");
+document.getElementById("output").innerHTML = text;
+  });
+
+
