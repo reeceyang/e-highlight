ed17bb16e847cde8406997f38bb029aeab2f9eec
Reece Yang <reeceyang@live.com>
2021-01-06 22:26:07 -0600

    make it work

--- e_highlight.js
+++ e_highlight.js
@@ -1,16 +1,8 @@
-console.log("script loaded");
-var line;
-var oldLength = 0;
-var input = document.getElementById("text");
-var out = document.getElementById("output");
 input.addEventListener("input", function() {
-    line = input.innerHTML;
-    out.innerHTML = out.innerHTML.substring(0, out.innerHTML.length-oldLength);
-    out.innerHTML += line;
-    oldLength = line.length;
   var text = document.getElementById("text").innerHTML;
-text.replace(/e/g, "<span style=\"background-color:#42f448\"\">e</span>");
-document.getElementById("output").innerHTML = text;
-  });
+  text = text.replace(/e/g, "<span style=\"background-color:#42f448\"\">e</span>");
+  console.log(text);
+  document.getElementById("output").innerHTML = text;
+});
 
 
