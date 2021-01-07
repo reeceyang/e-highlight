5f9c2d7296896057786f77329ff21bedc14bbcf1
reeceyang <reeceyang@live.com>
2021-01-07 13:28:45 -0600

    idk why I'm still working on this lmao

--- e_highlight.js
+++ e_highlight.js
@@ -1,8 +1,6 @@
 document.getElementById("text").addEventListener("input", function() {
-  var text = document.getElementById("text").value;
+  var text = document.getElementById("text").innerHTML;
   text = text.replace(/e/g, "<span style=\"background-color:#42f448\"\">e</span>");
   console.log(text);
   document.getElementById("output").innerHTML = text;
 });
-
-
