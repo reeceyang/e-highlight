3117585c9bedf8ffa089a8bf8d774d308e617cdf
Reece Yang <reeceyang@live.com>
2021-01-06 22:35:04 -0600

    Update e_highlight.js

--- e_highlight.js
+++ e_highlight.js
@@ -1,5 +1,5 @@
 document.getElementById("text").addEventListener("input", function() {
-  var text = document.getElementById("text").innerHTML;
+  var text = document.getElementById("text").value;
   text = text.replace(/e/g, "<span style=\"background-color:#42f448\"\">e</span>");
   console.log(text);
   document.getElementById("output").innerHTML = text;
